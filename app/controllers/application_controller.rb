1ec4d9b45583cd3afc0272f7a33f406cf3f60acb
George Ruiz <georuiz817@gmail.com>
2019-08-11 00:42:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < Sinatra::Base
 
 #render a form to create a new recipe: 
  get '/recipes/new' do
-2.	  erb :new
-3.	end
-4.	 
-5.	post '/articles' do
-6.	  @article = Article.create(:title => params[:title], :content => params[:content])
-7.	  redirect to "/articles/#{@article.id}"
-8.	end
+    erb :new
+ end
+	 
+	post '/articles' do
+	  @article = Article.create(:title => params[:title], :content => params[:content])
+	  redirect to "/articles/#{@article.id}"
+	end
 
 
 end
