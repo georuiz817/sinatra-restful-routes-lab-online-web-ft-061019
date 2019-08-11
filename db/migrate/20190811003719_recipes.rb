b7691e4b1c0472303c65aefc8cd543e4aff98777
George Ruiz <georuiz817@gmail.com>
2019-08-11 00:37:55 +0000

    Automatically backed up by Learn

--- db/migrate/20190811003719_recipes.rb
+++ db/migrate/20190811003719_recipes.rb
@@ -1,5 +1,9 @@
 class Recipes < ActiveRecord::Migration
   def change
-    
+    create_table Recipes do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end 
   end
 end
