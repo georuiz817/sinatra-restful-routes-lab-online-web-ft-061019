0518b305886951a25f88fee40b5989cc95d703f5
George Ruiz <georuiz817@gmail.com>
2019-08-11 00:40:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < Sinatra::Base
   end
 
 #render a form to create a new recipe: 
+ get '/recipes/new' do
+2.	  erb :new
+3.	end
+4.	 
+5.	post '/articles' do
+6.	  @article = Article.create(:title => params[:title], :content => params[:content])
+7.	  redirect to "/articles/#{@article.id}"
+8.	end
 
 
 end
